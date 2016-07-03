087bd4af42407b541ba016c0fb6ed1d276f1864f
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-07-03 05:14:50 +0200

    Create Files

---
+++ private-router.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/new-data', function (req, res) {
+
+});
+
+router.get('/queue', function (req, res) {
+
+});
+
+router.post('/status', function (req, res) {
+
+});
+
+module.exports = router;
---
+++ public-router.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/status', function (req, res) {
+
+});
+
+router.get('/queue', function (req, res) {
+
+});
+
+router.post('/coffee', function (req, res) {
+
+});
+
+module.exports = router;
