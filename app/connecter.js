20617744726a4f54b1ab31ada0c8e6e1cc85c119
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-07-05 01:01:00 +0200

    Implement some connecter.js functions

---
+++ app/connecter.js
@@ -0,0 +1,35 @@
+var redis = require('redis');
+
+var client = redis.createClient(process.env.PORT || 6379);
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+exports.setNewData = function (newData, next) {
+  if(newData === 'NONE' || newData === 'COFFEE' || newData === 'STATUS' || newData === 'BOTH'){
+    client.set('new-data', newData, next);
+  }
+  else {
+    console.log(newData + ' is not a possible value');
+  }
+}
+
+exports.setStatusData = function (beans, water, next) {
+  client.set('beans', beans, function () {
+    client.set('water', water, next);
+  });
+}
+
+exports.getStatusData = function (next) {
+  client.get('beans', function(err, reply){
+    if(err){
+      console.log('Error 'err);
+    }
+    var data = {'beans' : reply};
+    client.get('water', function (err, reply) {
+      data.water = reply;
+      return data;
+    });
+  });
+}
---
+++ app/private-router.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/new-data', function (req, res) {
+
+});
+
+router.get('/queue', function (req, res) {
+
+});
+
+router.post('/status', function (req, res) {
+
+});
+
+router.post('/finished', function (req, res) {
+
+});
+
+module.exports = router;
