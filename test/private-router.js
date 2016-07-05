dece51ed530bc3754cb2bd6c77623d2535966fd2
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-07-06 01:22:17 +0200

    Add Unit Test for /queue Route in private-route.js

--- test/private-router.js
+++ test/private-router.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var request = require('request');
+var connecter = require('../app/connecter.js');
 
 describe('Private API Router', function () {
   describe('GET /new-data', function () {
@@ -19,6 +20,32 @@ describe('Private API Router', function () {
 
     it('Sends the right keyword', function () {
       expect(value).to.deep.equal({"new-data": "BOTH"});
+    });
+  });
+
+  describe('GET /queue', function () {
+
+    var value;
+    var url = 'http://localhost:3000/private/queue';
+
+    before(function (done) {
+      connecter.addToQueue(4, function () {
+        connecter.addToQueue(3, function () {
+          connecter.addToQueue(5, function () {
+            request(url, function (err, response, body) {
+              if(err){
+                console.log('Test Error ' + err);
+              }
+              value = JSON.parse(body);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+    it('Sends the queue', function () {
+      expect(value).to.deep.equal({"queue": ["4","3","5"]});
     });
   });
 });
