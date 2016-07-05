57eb62e3f258fb4777c3af551e9b261a06d83ac4
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-07-06 01:42:02 +0200

    Add Body-Parser

--- app/index.js
+++ app/index.js
@@ -2,3 +2,12 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var server = app.listen(port);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var pubApi = require('../app/public-router.js');
+var priApi = require('../app/private-router.js');
+
+app.use('/public', pubApi);
+app.use('/private', priApi);
