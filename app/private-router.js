97b9096fb0a1de968e212d7845e98269883b0653
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-07-06 00:36:49 +0200

    Add /queue Route to private-router.js

--- app/private-router.js
+++ app/private-router.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var connecter = require('../app/connecter.js');
 
 router.get('/new-data', function (req, res) {
-  
+  connecter.getNewData(function (data) {
+    res.json({"new-data" : data});
+  })
 });
 
 router.get('/queue', function (req, res) {
+  connecter.getQueueLength(function (length) {
+    var queue = [];
 
+    function repeatPop(i,max) {
+      console.log(i);
+      if(i <= max){
+        connecter.popFrontOfQueue(function (coffee) {
+          queue[i] = coffee;
+          i++;
+          repeatPop(i,max);
+        });
+      }
+      else {
+        res.json({"queue" : queue});
+      }
+    }
+
+    repeatPop(0,length -1);
+  })
 });
 
 router.post('/status', function (req, res) {
